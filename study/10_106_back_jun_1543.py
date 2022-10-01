211d1498771ded0bcd85315bf000e7406281c0f9
sony-jun <warrise@naver.com>
2022-10-01 16:21:57 +0900

    중간저장

--- study/10_106_back_jun_1543.py
+++ study/10_106_back_jun_1543.py
@@ -1,8 +1,20 @@
-'''
+"""
 n = input()
 c = input()
 print(n.count(c)) 
-'''
+"""
+# 통과 못하는 코드
 n = input()
 c = input()
 cnt = 0
+b = n
+
+while True:
+    if c not in n:
+        break
+    if c in n:
+        cnt += 1
+        b = n.replace(c, "", cnt)
+        if c not in b:
+            break
+print(cnt)
---
+++ study/10_107_back_jun_2615.py
@@ -0,0 +1,6 @@
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+n, m = map(int, input().split())
+
+graph = [list(map(int, input())) for _ in range(n)]
