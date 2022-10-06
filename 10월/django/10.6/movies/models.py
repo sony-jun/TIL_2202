2be6e4af0ffde88121c69d9a1773ad0d55554ca8
sony-jun <warrise@naver.com>
2022-10-06 17:32:19 +0900

    장고_1006

---
+++ 10월/django/10.6/movies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=40)
+    summary = models.TextField()
+    running_time = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
