6ee497fc99a333c420773f27033be7452eff5da7
sony-jun <warrise@naver.com>
2022-10-06 23:06:27 +0900

    스터디_미로and지뢰찾기

--- study/10_099_back_jun_9325.py
+++ study/10_099_back_jun_9325.py
@@ -1,10 +1,27 @@
+t = int(input()) # 횟수
+for _ in range(t):
+    car = int(input()) # 차 가격
+    options = int(input()) # 옵션의 개수
+    cnt = car # 총 가격의 합이니까 기본값이 차 가격으로 설정
+    for i in range(options): # 옵션의 개수가 0일 경우, for문이 돌아가지 않음
+        a,b = map(int,input().split()) # 옵션개수, 옵션 가격 받음
+        c = a*b # 두개 곱함
+        cnt += c # 더해줌
+    print(cnt) # 총 가격
+
+
+'''
 t = int(input())
 for _ in range(t):
     car = int(input())
-    options = int(input())
-    cnt = car
-    for i in range(options):
-        a,b = map(int,input().split())
-        c = a*b
-        cnt += c
-    print(cnt)
+    bupum = int(input())
+    if bupum ==0:
+        print(car)
+    else:
+        mum = car
+        for _ in range(bupum):
+            a,b = map(int,input().split())
+            c = a*b
+            mum += c
+        print(mum)
+'''
--- study/10_107_back_jun_2615.py
+++ study/10_107_back_jun_2615.py
@@ -1,6 +1,39 @@
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+import sys
 
-n, m = map(int, input().split())
+input = sys.stdin.readline
 
-graph = [list(map(int, input())) for _ in range(n)]
+dx = [0, 1, 1, -1] # 우 , 하 , 우하 , 우상
+dy = [1, 0, 1, 1]
+
+def omok():
+    for x in range(19):
+        for y in range(19):
+            if maps[x][y]:
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+
+                    stone = 1
+
+                    if not (0 <= nx < 19 and 0 <= ny < 19):
+                        continue
+                    while 0 <= nx < 19 and 0 <= ny < 19 and maps[x][y] == maps[nx][ny]:
+                        stone += 1
+                        if stone == 5:
+                            if 0 <= x - dx[i] and 0 <= y - dy[i] and maps[x][y] == maps[x - dx[i]][y - dy[i]]: # 앞쪽
+                                break # 같기때문에 6목 이상
+                            if nx + dx[i] < 19 and ny + dy[i] < 19 and maps[nx][ny] == maps[nx + dx[i]][ny + dy[i]]: # 뒷쪽
+                                break # 같기 때문에 6목 이상
+                            return maps[nx][ny], x + 1 , y + 1 
+                        nx += dx[i]
+                        ny += dy[i]
+    return 0, -1, -1
+maps = [list(map(int, input().split())) for _ in range(19)]
+
+color, x, y = omok()
+
+if not color:
+    print(0)
+else:
+    print(color)
+    print(x, y)
