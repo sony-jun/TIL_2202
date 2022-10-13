f3cb48ffdbc69bed75940512bcb39117ed038da3
sony-jun <warrise@naver.com>
2022-10-13 17:33:12 +0900

    회원정보수정

---
+++ 10월/django/10.13/review/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ["title", "content", "movie_name", "grade"]
+
+        labels = {
+           'title' : '리뷰 제목',
+           'content' : '리뷰 내용',
+           'movie_name' : '영화 제목',
+           'grade' : '평점',
+        }
