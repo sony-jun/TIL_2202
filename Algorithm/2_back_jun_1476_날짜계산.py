8d91409f4ec98050f8dd298fda36c0a1efddd583
sony-jun <warrise@naver.com>
2022-12-22 01:06:14 +0900

    til

--- Algorithm/2_back_jun_1476_날짜계산.py
+++ Algorithm/2_back_jun_1476_날짜계산.py
@@ -1,60 +1,10 @@
-"""
-1. 아이디어
-브루트포스로 다 돌려본다. N이 최대 50이므로 가능하다.
-한 위치에서 상하좌우와 바꿀 수 있지만 겹치므로 아래와 오른쪽만 계속해서 바꿔주면 된다.
-바꿔준 뒤, 전체 보드에서 먹을 수 있는 사탕의 최대 개수를 구한 뒤 다시 원상복구 해주고
-다음 걸로 넘어가 바꿔주고 확인하고를 반복하면 된다.
-
-2. 시간복잡도
-- O(N^4) : 50 ** 4 = 6250000 < 2억   -> 가능
-
-3. 자료구조
-N: int;
-board: str[][];
-maxCnt: int;
-"""
-import sys
-
-input = sys.stdin.readline
-
-N = int(input())
-board = [list(input()) for _ in range(N)]
-maxCnt = 0
-
-
-def check():
-    global maxCnt
-    for i in range(N):
-        cnt = 1  # 행을 검사
-        for j in range(1, N):
-            if board[i][j] == board[i][j - 1]:
-                cnt += 1
-                maxCnt = max(cnt, maxCnt)
-            else:
-                cnt = 1
-
-        cnt = 1  # 열을 검사
-        for j in range(1, N):
-            if board[j][i] == board[j - 1][i]:
-                cnt += 1
-                maxCnt = max(cnt, maxCnt)
-            else:
-                cnt = 1
-
-
-for i in range(N):
-    for j in range(N):
-        # 오른쪽과 바꿈
-        if j + 1 < N:
-            board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
-            check()
-            board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
-
-        # 아래쪽과 바꿈
-        if i + 1 < N:
-            board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
-            check()
-            board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
-
-
-print(maxCnt)
+e, s, m = map(int, input().split())  # 지구 태양 달의 정수값을 받으므로
+cnt = 1
+# 각각의 최소공배수 혹은 바이오 리듬을 생각하면 비교적 간단하게 알 수 있다
+while True:
+    if (
+        (cnt - e) % 15 == 0 and (cnt - s) % 28 == 0 and (cnt - m) % 19 == 0
+    ):  # 모든 값의 나머지가 0일때 년도를 알 수 있게 된다!
+        break
+    cnt += 1
+print(cnt)
